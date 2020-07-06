97811562157c008023f6a166f3fcabece8f66f47
lenny esquivel <lenny.esquivel.fs@gmail.com>
2020-07-06 16:35:47 -0500

    IT LIVES!

--- index.js
+++ index.js
@@ -27,22 +27,33 @@ client.on('message', (msg) => {
 
     const musicQueue = queue.get(msg.guild.id);
 
-    if(msg.content.toLowerCase().startsWith(`${prefix}rubot`)){
-        msg.reply("Hullo");
-    } else if (msg.content.toLowerCase().startsWith(`${prefix}play`)) {
-        execute(msg, musicQueue);
-        return;
-    }  else if (msg.content.toLowerCase().includes("fitter happier")) {
-        msg.channel.send('Fitter, happier \nMore productive \nComfortable \nNot drinking too much \nRegular exercise at the gym, three days a week \nGetting on better with your associate employee contemporaries \nAt ease \nEating well, no more microwave dinners and saturated fats \nA patient, better driver \nA safer car, baby smiling in back seat \nSleeping well, no bad dreams \nNo paranoia \nCareful to all animals, never washing spiders down the plughole \nKeep in contact with old friends, enjoy a drink now and then \nWill frequently check credit at moral bank, hole in wall \nFavours for favours, fond but not in love \nCharity standing orders on sundays, ring-road supermarket \nNo killing moths or putting boiling water on the ants \nCar wash, also on sundays \nNo longer afraid of the dark or midday shadows, nothing so ridiculously teenage and desperate \nNothing so childish \nAt a better pace, slower and more calculated \nNo chance of escape \nNow self-employed \nConcerned, but powerless \nAn empowered and informed member of societ, pragmatism not idealism \nWill not cry in public \nLess chance of illness \nTires that grip in the wet, shot of baby strapped in backseat \nA good memory \nStill cries at a good film \nStill kisses with saliva \nNo longer empty and frantic \nLike a cat \nTied to a stick \nThats driven into \nFrozen winter shit, the ability to laugh at weakness \nCalm, fitter, healthier and more productive \nA pig in a cage on antibiotics');
-    } else {
-        msg.reply('Please, enter a valid command.');
+    let command = msg.content.substring(0, msg.content.indexOf(" "));
+
+    if(msg.content.substring(msg.content.indexOf(" ")).length < 1 || msg.content.indexOf(" ") === null);
+        command = msg.content;
+
+    switch(command){
+        case `${prefix}rubot`:
+            msg.reply("Hullo");
+            break;
+        case `${prefix}play`:
+            execute(msg, musicQueue);
+            break;
+        case `${prefix}skip`:
+            skip(msg, musicQueue);
+            break;
+        case `${prefix}stop`:
+            stop(msg, musicQueue);
+            break;
+        case `${prefix}fitter`:
+            msg.channel.send('Fitter, happier \nMore productive \nComfortable \nNot drinking too much \nRegular exercise at the gym, three days a week \nGetting on better with your associate employee contemporaries \nAt ease \nEating well, no more microwave dinners and saturated fats \nA patient, better driver \nA safer car, baby smiling in back seat \nSleeping well, no bad dreams \nNo paranoia \nCareful to all animals, never washing spiders down the plughole \nKeep in contact with old friends, enjoy a drink now and then \nWill frequently check credit at moral bank, hole in wall \nFavours for favours, fond but not in love \nCharity standing orders on sundays, ring-road supermarket \nNo killing moths or putting boiling water on the ants \nCar wash, also on sundays \nNo longer afraid of the dark or midday shadows, nothing so ridiculously teenage and desperate \nNothing so childish \nAt a better pace, slower and more calculated \nNo chance of escape \nNow self-employed \nConcerned, but powerless \nAn empowered and informed member of societ, pragmatism not idealism \nWill not cry in public \nLess chance of illness \nTires that grip in the wet, shot of baby strapped in backseat \nA good memory \nStill cries at a good film \nStill kisses with saliva \nNo longer empty and frantic \nLike a cat \nTied to a stick \nThats driven into \nFrozen winter shit, the ability to laugh at weakness \nCalm, fitter, healthier and more productive \nA pig in a cage on antibiotics');
+            break;
     }
-    console.log(msg.content + " sent by " + msg.author.username);
 });
 
 async function execute(message, musicQueue) {
-    console.log(message.content.length);
-    const songName = message.content.substring(" ", message.content.length + 1);
+
+    const songName = message.content.substring(message.content.indexOf(" "), message.content.length + 1);
     console.log("SONG " + songName);
     const voiceChannel = message.member.voice.channel;
     if(!voiceChannel) return message.reply("Please enter a voice channel to crank that soulja boi.");
@@ -90,12 +101,11 @@ async function execute(message, musicQueue) {
         console.log(musicQueue.songs);
         return message.channel.send(`${song.title} has been added to the queueueueue.`);
 
-    } //Hacer switch para los comandos
+    }
 
 }
 
 function play(guild, song){
-
     let serverQueue = queue.get(guild.id);
     if(!song){
         serverQueue.voiceChannel.leave();
@@ -112,5 +122,19 @@ function play(guild, song){
     dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
 
     serverQueue.textChannel.send(`Now playing: \'${song.title}\'`);
-    
+}
+
+function skip(message, musicQueue) {
+    if(!message.member.voice.channel)
+        return message.reply("You need to be in a voice channel first");
+    if(!musicQueue)
+        return message.channel.send("Theres is no next song.");
+    musicQueue.connection.dispatcher.end();
+}
+
+function stop(message, musicQueue) {
+    if(!message.member.voice.channel)
+        return message.reply("You need to be in a voice channel first");
+    musicQueue.songs = [];
+    musicQueue.connection.dispatcher.end();
 }
